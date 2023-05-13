ca2b745031258f51b3bda06dd3b7ea047b5121eb
A-K-0-4-7 <ankit.v.kumar@capgemini.com>
2023-05-13 16:46:06 +0530

    first

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,25 @@
 package com.driver;
 
 public class Main {
-
+    public static class Product{
+        public int product(int x,int y)
+        {
+            return 1;
+        }
+        public int product(int x,int y,int z)
+        {
+            return 2;
+        }
+        public double product(double x,double y)
+        {
+            return 3;
+        }
+   }
+    public static void main(String[] args)
+    {
+        Product p = new Product();
+        int ans1 = p.product(1,1);
+        int ans2 = p.product(1,1,4);
+        double ans3 = p.product(1.0,1.0);
+    }
 }
